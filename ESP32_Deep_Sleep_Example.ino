c5e160a41afc78972efd404e677e2e28a7099f85
G6EJD <d.l.bird@sky.com>
2017-06-03 12:12:49 +0100

    Add files via upload

---
+++ ESP32_Deep_Sleep_Example.ino
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "sys/time.h"
+#include "sdkconfig.h"
+#include "esp_deep_sleep.h"
+#include <WiFi.h>
+
+#define GPIO_DEEP_SLEEP_DURATION     10  // sleep 10 seconds and then wake up
+#define LED_PIN                      2
+
+RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
+
+void setup(){
+  
+}
+
+void loop() {
+  WiFi.mode(WIFI_OFF);
+  btStop(); // btStart();
+  struct timeval now;
+  pinMode(LED_PIN, OUTPUT);      // set the LED pin mode
+  printf("Starting ESP32 after deep-sleep\n");
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  gettimeofday(&now, NULL);
+  printf("deep sleep (%lds since last reset, %lds since last boot)\n",now.tv_sec,now.tv_sec-last);
+  last = now.tv_sec;
+  esp_deep_sleep(1000000LL * GPIO_DEEP_SLEEP_DURATION);
+}
+
---
+++ ESP32_OLED_TEST_SHT22_02.ino
@@ -0,0 +1,64 @@
+// Include the correct display library
+// For a connection via I2C using Wire include
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "DHT.h"   //https://github.com/adafruit/DHT-sensor-library 
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+// or #include "SH1106.h" alis for `#include "SH1106Wire.h"`
+// For a connection via I2C using brzo_i2c (must be installed) include
+// #include <brzo_i2c.h> // Only needed for Arduino 1.6.5 and earlier
+// #include "SSD1306Brzo.h"
+// #include "SH1106Brzo.h"
+// For a connection via SPI include
+// #include <SPI.h> // Only needed for Arduino 1.6.5 and earlier
+// #include "SSD1306Spi.h"
+// #include "SH1106SPi.h"
+
+// Initialize the OLED display using Wire library
+SSD1306  display(0x3c, 5,4);
+
+// SH1106 display(0x3c, 5,4);
+// Create the DHT temperature and humidity sensor object 
+DHT dht1(0, DHT22); 
+
+float DHT22_t, DHT22_h;
+int xpos = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  dht1.begin();
+  DHT22_t = dht1.readTemperature(); 
+  DHT22_h = dht1.readHumidity(); 
+     
+  Serial.print("DHT22  "); 
+  Serial.print(DHT22_t,1); Serial.print(String(char(176))+"C  "); 
+  Serial.print(DHT22_h,1); Serial.println("%RH"); 
+  Serial.println(); 
+
+  // Initialising the UI will init the display too.
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+}
+
+void drawTempHumiDemo() {
+  DHT22_t = dht1.readTemperature(); 
+  DHT22_h = dht1.readHumidity(); 
+  display.setFont(ArialMT_Plain_24);
+  display.setTextAlignment(TEXT_ALIGN_CENTER); // The coordinates define the center of the screen!
+  display.drawString(64,0,String(DHT22_t,1)+"°C");
+  display.drawString(64,30,String(DHT22_h,1)+"% rh");
+  display.setTextAlignment(TEXT_ALIGN_LEFT); // The coordinates define the center of the screen!
+  display.drawString(xpos%128,40,"-");
+}
+
+void loop() {
+  display.clear();  // clear the display
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  drawTempHumiDemo();
+  display.display();
+  delay(1500);
+  xpos = xpos + 2;
+}
+
+
